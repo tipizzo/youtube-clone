f02bc20dde66fb9e661da2189c0cae88dc19bf71
KABANGA David <Davkabanga96@gmail.com>
2024-11-05 19:16:56 +0200

    Setting up the Youtube Data API

--- src/Components/Feed/Feed.jsx
+++ src/Components/Feed/Feed.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Feed.css'
 import thumbnail1 from '../../assets/thumbnail1.png'
 import thumbnail2 from '../../assets/thumbnail2.png'
@@ -9,8 +9,19 @@ import thumbnail6 from '../../assets/thumbnail6.png'
 import thumbnail7 from '../../assets/thumbnail7.png'
 import thumbnail8 from '../../assets/thumbnail8.png'
 import { Link } from 'react-router-dom'
+import { API_KEY } from '../../data'
 
-const Feed = () => {
+const Feed = ({ category }) => {
+
+    const [data, setData] = useState([]);
+
+    const fetchData = async () => {
+        const videoListUrl = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=20&regionCode=US&videoCategoryId=${category}&key=${API_KEY}`
+        await fetch(videoListUrl)
+        .then(response => response.json())
+        .then(data=>setData(data.items))
+    }
+
   return (
     <div className='feed'>
         <Link to={`video/20/4521`} className='card'>
