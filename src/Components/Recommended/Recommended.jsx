3ded1a936ec6727a0004181c22e7d4957b7a42dc
KABANGA David <Davkabanga96@gmail.com>
2024-11-06 19:21:50 +0200

    Implementing the recommended component

--- src/Components/Recommended/Recommended.jsx
+++ src/Components/Recommended/Recommended.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Recommended.css'
 import thumbnail1 from '../../assets/thumbnail1.png'
 import thumbnail2 from '../../assets/thumbnail2.png'
@@ -8,74 +8,40 @@ import thumbnail5 from '../../assets/thumbnail5.png'
 import thumbnail6 from '../../assets/thumbnail6.png'
 import thumbnail7 from '../../assets/thumbnail7.png'
 import thumbnail8 from '../../assets/thumbnail8.png'
+import { API_KEY } from '../../data'
 
-const Recommended = () => {
+const Recommended = ({ categoryId }) => {
+
+    const [apiData, setApiData] = useState([]);
+
+    const fetchData = async () => {
+
+        const relatedVideoUrl = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=US&videoCategoryId=${categoryId}&key=${API_KEY}`
+        await fetch(relatedVideoUrl)
+        .then(response => response.json())
+        .then(data => setApiData(data.items))
+
+    }
+
+    useEffect(() => {
+        fetchData();
+    }, [])
+
   return (
     <div className='recommended'>
-        <div className='side-video-list'>
-            <img src={thumbnail1} alt='' />
-            <div className='vid-info'>
-                <h4>Best channel that help you to be a web developer</h4>
-                <p>GreatStack</p>
-                <p>199K Views</p>
-            </div>
-        </div>
-        <div className='side-video-list'>
-            <img src={thumbnail2} alt='' />
-            <div className='vid-info'>
-                <h4>Best channel that help you to be a web developer</h4>
-                <p>GreatStack</p>
-                <p>199K Views</p>
-            </div>
-        </div>
-        <div className='side-video-list'>
-            <img src={thumbnail3} alt='' />
-            <div className='vid-info'>
-                <h4>Best channel that help you to be a web developer</h4>
-                <p>GreatStack</p>
-                <p>199K Views</p>
-            </div>
-        </div>
-        <div className='side-video-list'>
-            <img src={thumbnail4} alt='' />
-            <div className='vid-info'>
-                <h4>Best channel that help you to be a web developer</h4>
-                <p>GreatStack</p>
-                <p>199K Views</p>
-            </div>
-        </div>
-        <div className='side-video-list'>
-            <img src={thumbnail5} alt='' />
-            <div className='vid-info'>
-                <h4>Best channel that help you to be a web developer</h4>
-                <p>GreatStack</p>
-                <p>199K Views</p>
-            </div>
-        </div>
-        <div className='side-video-list'>
-            <img src={thumbnail6} alt='' />
-            <div className='vid-info'>
-                <h4>Best channel that help you to be a web developer</h4>
-                <p>GreatStack</p>
-                <p>199K Views</p>
-            </div>
-        </div>
-        <div className='side-video-list'>
-            <img src={thumbnail7} alt='' />
-            <div className='vid-info'>
-                <h4>Best channel that help you to be a web developer</h4>
-                <p>GreatStack</p>
-                <p>199K Views</p>
-            </div>
-        </div>
-        <div className='side-video-list'>
-            <img src={thumbnail8} alt='' />
-            <div className='vid-info'>
-                <h4>Best channel that help you to be a web developer</h4>
-                <p>GreatStack</p>
-                <p>199K Views</p>
-            </div>
-        </div>
+        {apiData.map((item, index) => {
+            return (
+                <div key={index} className='side-video-list'>
+                    <img src={thumbnail1} alt='' />
+                    <div className='vid-info'>
+                        <h4>Best channel that help you to be a web developer</h4>
+                        <p>GreatStack</p>
+                        <p>199K Views</p>
+                    </div>
+                </div>
+            )
+        })}
+        
     </div>
   )
 }
