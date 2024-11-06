a328dbe22416bf1c4f5731d73d0bc2cc8b517db9
KABANGA David <Davkabanga96@gmail.com>
2024-11-06 15:22:35 +0200

    Setting up the Play video component

--- src/Components/PlayVideo/PlayVideo.jsx
+++ src/Components/PlayVideo/PlayVideo.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './PlayVideo.css'
 import video1 from '../../assets/video.mp4'
 import like from '../../assets/like.png'
@@ -7,13 +7,30 @@ import share from '../../assets/share.png'
 import save from '../../assets/save.png'
 import jack from '../../assets/jack.png'
 import user_profile from '../../assets/user_profile.jpg'
+import { API_KEY, value_converter } from '../../data'
 
 const PlayVideo = ({ videoId }) => {
+
+    const [apiData, setApiData] = useState(null)
+
+    const fetchVideoData = async () => {
+        // Fetching videos Data
+
+        const videoDetailsUrl = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&id=${videoId}&key=${API_KEY}`;
+        await fetch(videoDetailsUrl)
+        .then(response => response.json())
+        .then(data => setApiData(data.items[0]))
+    }
+
+    useEffect(() => {
+        fetchVideoData();
+    }, [])
+
   return (
     <div className='play-video'>
       {/* <video src={video1} controls autoPlay muted></video> */}
-      <iframe  src={`https://www.youtube.com/embed/${videoId}`} frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-      <h3>Best YouTube Channel To Learn Web Development</h3>
+      <iframe  src={`https://www.youtube.com/embed/${videoId}?autoplay=1`} frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+      <h3>{apiData?apiData.snippet.title:"Title Here"}</h3>
       <div className='play-video-info'>
         <p>1525 Views &bull; 2 days ago</p>
         <div>
--- src/Pages/Video/Video.jsx
+++ src/Pages/Video/Video.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import './Video.css'
 import PlayVideo from '../../Components/PlayVideo/PlayVideo'
 import Recommended from '../../Components/Recommended/Recommended'
+import { useParams } from 'react-router-dom'
 
 const Video = () => {
 
