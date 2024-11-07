1f1e1a05ba1e83df1cbcc2225bbc2031baaf4d1e
KABANGA David <Davkabanga96@gmail.com>
2024-11-07 10:54:34 +0200

    Making the project responsive

--- src/Components/PlayVideo/PlayVideo.jsx
+++ src/Components/PlayVideo/PlayVideo.jsx
@@ -9,8 +9,11 @@ import jack from '../../assets/jack.png'
 import user_profile from '../../assets/user_profile.jpg'
 import { API_KEY, value_converter } from '../../data'
 import moment from 'moment'
+import { useParams } from 'react-router-dom'
 
-const PlayVideo = ({ videoId }) => {
+const PlayVideo = () => {
+
+    const {videoId} = useParams();
 
     const [apiData, setApiData] = useState(null)
     const [channelData, setChannelData] = useState(null)
@@ -43,7 +46,7 @@ const PlayVideo = ({ videoId }) => {
 
     useEffect(() => {
         fetchVideoData();
-    }, [])
+    }, [videoId])
 
     useEffect(() => {
         fetchOtherData();
--- src/Components/Recommended/Recommended.jsx
+++ src/Components/Recommended/Recommended.jsx
@@ -8,19 +8,39 @@ import thumbnail5 from '../../assets/thumbnail5.png'
 import thumbnail6 from '../../assets/thumbnail6.png'
 import thumbnail7 from '../../assets/thumbnail7.png'
 import thumbnail8 from '../../assets/thumbnail8.png'
-import { API_KEY_2 } from '../../data'
+import {  value_converter } from '../../data'
 
-const Recommended = () => {
+const Recommended = ({ categoryId }) => {
+
+    const [apiData, setApiData] = useState([])
+
+    const fetchData = async () => {
+        const relatedVideoUrl = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=5&regionCode=CD&videoCategoryId=${categoryId}&key=${API_KEY}`; // Add the API_KEY from where it's stored
+        await fetch(relatedVideoUrl)
+        .then(res => res.json())
+        .then(data => setApiData(data.items))
+    }
+
+    useEffect(() => {
+        fetchData();
+    }, [])
+
   return (
-        <div className='recommended'>
-            <div className='side-video-list'>
-                <img src={thumbnail1} alt='' />
-                <div className='vid-info'>
-                    <h4>Best channel that helps you to be a web developer</h4>
-                    <p>GreatStack</p>
-                    <p>199K Views</p>
-                </div>
-            </div>
+    
+        <div  className='recommended'>
+            {apiData.map((item, index) => {
+                return (
+                <Link to={`/video/${item.snippet.categoryId}/${item.id}`} key={index} className='side-video-list'>
+                    <img src={item.snippet.thumbnails.medium.url} alt='' />
+                    <div className='vid-info'>
+                        <h4>{item.snippet.title}</h4>
+                        <p>{item.snippet.channelTitle}</p>
+                        <p>{value_converter(item.statistics.viewCount)} Views</p>
+                    </div>
+                </Link>
+                )
+        })}
+            {/*
             <div className='side-video-list'>
                 <img src={thumbnail2} alt='' />
                 <div className='vid-info'>
@@ -76,7 +96,8 @@ const Recommended = () => {
                     <p>GreatStack</p>
                     <p>199K Views</p>
                 </div>
-            </div>        
+            </div>       
+*/} 
         </div>
   )
 }
